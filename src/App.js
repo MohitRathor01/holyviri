57b16a9cf0bb24b916f53a1b9a414318d9d95328
Krishna Singh Bhainsora <krishnabhainsora@gmail.com>
2024-06-18 16:23:00 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HvtCarousel from './pages/HvtCarousel';
+
 import HvtFooter from './pages/HvtFooter';
 import { HvtHeader } from './pages/HvtHeader';
 
--- src/pages/HvtCarousel.jsx
+++ src/pages/HvtCarousel.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import '../pagesCss/HvtCarousel.css'
+import React, { useState, useEffect } from 'react';
+import '../pagesCss/HvtCarousel.css';
 
 const HvtCarousel = () => {
-    return (<>
-    </>
-    )
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    "/images/new1.jpeg",
+    "/images/new2.jpg",
+    "/images/yamunaa.jpg",
+    "/images/new.jpg",
+    "/images/new4.jpg",
+    "/images/new5.jpg"
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, 5000); // Change slide every 5 seconds
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <main className="new-sec">
+      <ul className="slide">
+        {images.map((image, index) => (
+          <li key={index} className={`item ${index === currentIndex ? 'slide-animation' : ''}`}>
+            <img src={image} alt={`Image ${index + 1}`} className="img-fluid" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
-export default HvtCarousel
+export default HvtCarousel;
