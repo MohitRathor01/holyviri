62738f67a15b366a93d03af97067d1062b33c802
Krishna Singh Bhainsora <krishnabhainsora@gmail.com>
2024-06-18 15:16:07 +0530

    header or footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import HvtCarousel from './pages/HvtCarousel';
+import HvtFooter from './pages/HvtFooter';
+import { HvtHeader } from './pages/HvtHeader';
 
 function App() {
   return (<>
+  <HvtHeader/>
+  <HvtCarousel/>
+  <HvtFooter/>
   </>
   )
 }
