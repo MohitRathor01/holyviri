b6862ce0028e8bb8d6d4a353691fac880ce963d4
MohitRathor01 <rathormohit547@gmail.com>
2024-06-18 06:06:24 -0700

    layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import HvtCarousel from './pages/HvtCarousel';
 import HvtFooter from './pages/HvtFooter';
 import { HvtHeader } from './pages/HvtHeader';
+import HvtWwr from './pages/HvtWwr';
 import Hvttourcard from './pages/Hvttourcard';
 
 function App() {
   return (<>
 <HvtHeader/>
 <HvtCarousel/>
+<HvtWwr/>
 <Hvttourcard/>
 <HvtFooter/>
   </>
